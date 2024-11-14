44690a019a74dbcd67291945e1f6ed3676691b05
Mangesh Namdeo Gosavi <148337560+Mangesh-Gosavi@users.noreply.github.com>
2024-11-14 14:43:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,39 @@
-import { useEffect, useState } from 'react';
-import './CSS/App.css'
-import Header from './Header'
-import Loader from '../src/Loader';
-import Navigation from './Navigation'
-import { Outlet } from 'react-router-dom'
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../src/Home";
+import Users from "../src/Users";
+import Addprod from "../src/Addprod";
+import Product from "../src/Product";
+import Orderitem from "../src/Items";
+import Updatestock from "../src/Stock";
+import Login from "../src/Login";
+import Dashboard from "../src/Chart";
+import Ordersdetail from "../src/Orders";
+import Review from "../src/Review";
+import Signup from "../src/Signup";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false); 
-    }, 2000); 
-
-  }, []);
-
   return (
-    <>
-    <div className='header'><Header/></div>
-    {loading ? (
-        <Loader/>
-      ) : (<div><div className='center'>
-       <div className='display'><Navigation/></div>
-        <div className='right'><Outlet/></div>
-    </div>
-    <div className='left'>
-       <div className='display'><Navigation/></div>
-        <div className='right'><Outlet/></div>
-    </div></div>)}
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        
+        {/* Home Route with Nested Routes */}
+        <Route path="/home" element={<Home />}>
+          <Route path="users" element={<Users />} />
+          <Route path="addproduct" element={<Addprod />} />
+          <Route path="product" element={<Product />} />
+          <Route path="review" element={<Review />} />
+          <Route path="orderitem" element={<Orderitem />} />
+          <Route path="orderdetails" element={<Ordersdetail />} />
+          <Route path="updatestock" element={<Updatestock />} />
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
