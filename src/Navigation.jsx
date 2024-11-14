f78a56c943aac42ccf531705037dd09c46ece4a0
Mangesh Namdeo Gosavi <148337560+Mangesh-Gosavi@users.noreply.github.com>
2024-11-14 14:44:46 +0530

    Update Navigation.jsx

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -6,14 +6,14 @@ function Navigation(){
     return (
       <div className="navigation">
         <ul className="list">
-          <Link className='link' to="/Dashboard"><li>Dashboard</li></Link>
-          <Link className='link' to="/Users"><li>Users</li></Link>
-          <Link className='link' to="/AddProduct"><li>Add Product</li></Link>
-          <Link className='link' to="/Product"><li>Display Product</li></Link>
-          <Link className='link' to="/Review"><li>Product Reviews</li></Link>
-          <Link className='link' to="/Updatestock"><li>Update Product Stock</li></Link>
-          <Link className='link' to="/Orderitem"><li>Ordered Items</li></Link>
-          <Link className='link' to="/Orderdetails"><li>Orders Details</li></Link>
+          <Link className='link' to="/Home/dashboard"><li>Dashboard</li></Link>
+          <Link className='link' to="/Home/users"><li>Users</li></Link>
+          <Link className='link' to="/Home/addProduct"><li>Add Product</li></Link>
+          <Link className='link' to="/Home/product"><li>Display Product</li></Link>
+          <Link className='link' to="/Home/review"><li>Product Reviews</li></Link>
+          <Link className='link' to="/Home/updatestock"><li>Update Product Stock</li></Link>
+          <Link className='link' to="/Home/orderitem"><li>Ordered Items</li></Link>
+          <Link className='link' to="/Home/orderdetails"><li>Orders Details</li></Link>
         </ul>
       </div>
     )
